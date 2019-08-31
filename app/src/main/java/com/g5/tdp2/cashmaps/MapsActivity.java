04d01f0468b8248060a4d5832fa8cbaa9d4af079
zaragozamartin91 <zaragozamartin91@gmail.com>
2019-08-31 13:41:26 -0300

    Se refactorizan algunas partes del flujo del mapa. Se renombra el mensaje de error de geolocalizacion para ajustarlo a los requerimientos del profesor

--- app/src/main/java/com/g5/tdp2/cashmaps/MapsActivity.java
+++ app/src/main/java/com/g5/tdp2/cashmaps/MapsActivity.java
@@ -19,113 +19,115 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Optional;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
-	private GoogleMap mMap;
-	private static final String[] INITIAL_PERMS={
-			Manifest.permission.ACCESS_FINE_LOCATION
-	};
-	private static final int INITIAL_REQUEST=1337;
-	private static final int LOCATION_REQUEST=INITIAL_REQUEST;
+    private GoogleMap mMap;
+    private static final String[] INITIAL_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private static final int INITIAL_REQUEST = 1337;
+    private static final int LOCATION_REQUEST = INITIAL_REQUEST;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_maps);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-		if (!canAccessLocation()) {
-			requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
-		}
+        if (canAccessLocation()) {
+            launchMap();
+        } else {
+            requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
+        }
+    }
 
-		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
-		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.map);
-		mapFragment.getMapAsync(this);
-	}
+    private void launchMap() {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-	private boolean canAccessLocation() {
-		return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
-	}
+    private boolean canAccessLocation() {
+        return hasPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+    }
 
-	private boolean hasPermission(String perm) {
-		return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(perm));
-	}
+    private boolean hasPermission(String perm) {
+        return PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm);
+    }
 
-	private void notAccessLocation() {
-		Toast.makeText(this, R.string.no_access_location, Toast.LENGTH_LONG).show();
+    private void notAccessLocation() {
+        Toast.makeText(this, R.string.no_access_location, Toast.LENGTH_LONG).show();
         finish();
     }
 
-	@Override
-	public void onLocationChanged(Location location) {
-		Log.d("CHANGED", "LOCATION UPDATED");
-	}
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("CHANGED", "LOCATION UPDATED");
+    }
 
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {}
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
 
-	@Override
-	public void onProviderEnabled(String provider) {}
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
 
-	@Override
-	public void onProviderDisabled(String provider) {}
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
 
-	@Override
-	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-		switch(requestCode) {
-			case LOCATION_REQUEST:
-				if (canAccessLocation()) {
-					setUpMap();
-				}
-				else {
-					notAccessLocation();
-				}
-				break;
-		}
-	}
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST) {
+            if (canAccessLocation()) {
+                launchMap();
+            } else {
+                notAccessLocation();
+            }
+        }
+    }
 
-	private void centerAndMarkLocation(Location location) {
-		if (mMap != null && location != null){
-			LatLng latLngLocation = new LatLng(location.getLatitude(), location.getLongitude());
-			mMap.addMarker(new MarkerOptions().position(latLngLocation).title("Your Location"));
-			mMap.moveCamera(CameraUpdateFactory.newLatLng(latLngLocation));
-		}
-	}
+    private void centerAndMarkLocation(Location location) {
+        Optional.ofNullable(mMap).ifPresent(m -> {
+            LatLng latLngLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            m.addMarker(new MarkerOptions().position(latLngLocation).title("Your Location"));
+            m.moveCamera(CameraUpdateFactory.newLatLng(latLngLocation));
+        });
+    }
 
-	private void setUpMap() {
-		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-		try {
-			Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        setUpMap();
+    }
 
-			if (myLocation != null) {
+    private void setUpMap() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        try {
+            Optional<Location> location = Optional.ofNullable(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+
+            if (location.isPresent()) {
                 Log.d("location-get", "NOT NULL");
-                centerAndMarkLocation(myLocation);
-			} else {
-				Log.d("location-get", "NULL");
-				locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-			}
-		}
-		catch (SecurityException se) {
-			Log.d("location-get", "SE CAUGHT");
-			se.printStackTrace();
-		}
-	}
-
-	/**
-	 * Manipulates the map once available.
-	 * This callback is triggered when the map is ready to be used.
-	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
-	 * we just add a marker near Sydney, Australia.
-	 * If Google Play services is not installed on the device, the user will be prompted to install
-	 * it inside the SupportMapFragment. This method will only be triggered once the user has
-	 * installed Google Play services and returned to the app.
-	 */
-	@Override
-	public void onMapReady(GoogleMap googleMap) {
-		mMap = googleMap;
-		if (canAccessLocation()) {
-			mMap.setMyLocationEnabled(true);
-			setUpMap();
-		}
-	}
+                centerAndMarkLocation(location.get());
+            } else {
+                Log.d("location-get", "NULL");
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            }
+        } catch (SecurityException se) {
+            Log.d("location-get", "SE CAUGHT");
+            se.printStackTrace();
+        }
+    }
 }
